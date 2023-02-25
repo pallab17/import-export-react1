c20d8be6883c1c94d7f4331f1b5c35162e5d2270
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-25 20:00:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import pi, { doublePi, triplePi } from "./math.js";
-//
+/* import pi, { doublePi, triplePi } from "./math.js";
+eikhane pi= default export mane sudhu ektai value export hoye asche
+i.e. const pi = 3.1415962; from math.js file
+pi naam ta variable i.e. default export jodi hoye thake
+jeikhane amra import korchi seikhane we can gib it any name ami chaile pi na likhe pc o naam dite pari
+
+eibar { doublePi, triplePi } hocche normal export or non default export jeta ekta r beshi value export hoye asche
+i.e. 
+
+
+
+
+
+*/
 
 ReactDOM.render(
   <ul>
